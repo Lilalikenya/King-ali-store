4a5be58780a6891c6a56f351367ce0da5c5f1b02
Lilalikenya <lilalikenya@gmail.com>
2026-08-10 03:05:14 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,11 @@
+const KING_ALI_PRODUCTS = [
+  {
+    id: "KA-GOLF-001",
+    name: "Second Hand Golf Clubs",
+    category: "Golf Equipment",
+    price: 30000,
+    stock: 1,
+    image: "images/second-hand-golf-clubs.jpg",
+    description: "Second-hand golf club set that's ready for the course? 🔥 Complete Golf Set including Driver, 3 Wood, Irons, Putter and Stand Golf Bag."
+  }
+];
